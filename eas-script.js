b43b8131a8f80bdc68d7e49167513b54c66f7f26
ralph orteza <ralphlorteza@gmail.com>
2022-06-27 14:48:34 -0700

    Add function to clear grid

--- eas-script.js
+++ eas-script.js
@@ -1,20 +1,25 @@
 const container = document.querySelector('.container');
 const DEFAULT_SIZE = 16;
+let size = DEFAULT_SIZE;
 const btnSize = document.querySelector('.btn-size');
+const btnClear = document.querySelector('.btn-clear');
 
 // Creates a 16x16 grid of divs inside 'container' div.
-function makeGrid() {
-  let gridSize = changeSize(DEFAULT_SIZE);
-  for (let i=0; i<gridSize; i++) {
+function makeGrid(DEFAULT_SIZE) {
+  
+  for (let i=0; i<DEFAULT_SIZE; i++) {
     let row = document.createElement('div');
     row.classList = "row";
-    for (let j=0; j<gridSize; j++) {
+    for (let j=0; j<DEFAULT_SIZE; j++) {
       const box = document.createElement('div');
       box.classList = "box";
       row.appendChild(box);
     }
     container.appendChild(row);
   }
+
+  const gridBoxList = document.querySelectorAll('.box');
+  gridBoxList.forEach(box => {box.addEventListener("mouseover", changeColor)});
 
   
 }
@@ -26,21 +31,35 @@ function changeColor(event) {
 }
 
 
-btnSize.addEventListener('click', changeSize);
+
 function changeSize(DEFAULT_SIZE) {
   let text;
   let sizePrompt = prompt("Change size to: ", DEFAULT_SIZE.toString());
   sizePrompt = sizePrompt.trim();
-  while (parseInt(sizePrompt) > 100 || sizePrompt == null || parseInt(sizePrompt) < 0 || sizePrompt == "") {
+  sizePrompt = parseInt(sizePrompt);
+  while (sizePrompt > 100 || sizePrompt == null || sizePrompt < 0 || sizePrompt == "") {
     text = "Size must not exceed 100"
     sizePrompt = prompt("Change size to: ", DEFAULT_SIZE.toString());
+    sizePrompt = parseInt(sizePrompt.trim());
   }
+  size = sizePrompt;
+  clearGrid();
+}
 
-  return parseInt(sizePrompt);
+function clearGrid() {
+  let getSize = size;
+  while (container.firstChild) {
+    container.firstChild.remove();
+  } 
+  makeGrid(getSize);
 }
 
 
 
-makeGrid();
-const gridBoxList = document.querySelectorAll('.box');
-gridBoxList.forEach(box => {box.addEventListener("mouseover", changeColor)});
+
+
+
+makeGrid(DEFAULT_SIZE);
+btnSize.addEventListener('click', changeSize);
+btnClear.addEventListener('click', clearGrid);
+
